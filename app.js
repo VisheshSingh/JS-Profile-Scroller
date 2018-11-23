8b6fb6a9aa1d8f50f9db48282e47bc6e8b5062ec
Vishesh <visheshsingh16@gmail.com>
2018-11-23 12:37:17 -0500

    Reloading if profile not found

--- app.js
+++ app.js
@@ -35,22 +35,26 @@ function nextProfile() {
   // GET THE CURRENT PROFILE
   const currentProfile = profiles.next().value;
 
-  const profile = document.getElementById("profileDisplay");
-  profile.innerHTML = `
-    <ul class="list-group">
-        <li class="list-group-item">Name: ${currentProfile.name}</li>    
-        <li class="list-group-item">Age: ${currentProfile.age}</li>    
-        <li class="list-group-item">Location: ${
-          currentProfile.location
-        }</li>    
-        <li class="list-group-item">Preference: ${
-          currentProfile.gender
-        } looking for ${currentProfile.lookingFor}</li>     
-    </ul>`;
+  if (currentProfile !== undefined) {
+    const profile = document.getElementById("profileDisplay");
+    profile.innerHTML = `
+      <ul class="list-group">
+          <li class="list-group-item">Name: ${currentProfile.name}</li>    
+          <li class="list-group-item">Age: ${currentProfile.age}</li>    
+          <li class="list-group-item">Location: ${
+            currentProfile.location
+          }</li>    
+          <li class="list-group-item">Preference: ${
+            currentProfile.gender
+          } looking for ${currentProfile.lookingFor}</li>     
+      </ul>`;
 
-  document.getElementById("imageDisplay").innerHTML = `<img src="${
-    currentProfile.image
-  }">`;
+    document.getElementById("imageDisplay").innerHTML = `<img src="${
+      currentProfile.image
+    }">`;
+  } else {
+    window.location.reload();
+  }
 }
 
 // ITERATOR
